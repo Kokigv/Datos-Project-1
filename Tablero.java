7b71a7dfb4b6de899dfa82820ec51b1bfba13b1c
Ahenao16 <ahenao@estudiantec.cr>
2023-09-22 17:53:28 -0600

    Feature: Guardado de Cuadrados,lineas y dots en linked lists

--- Tablero.java
+++ Tablero.java
@@ -1,172 +1,379 @@
 import javax.swing.*;
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Tablero{
+public class Tablero {
 
-JFrame window;
-JPanel panel_juego;
+    JFrame window;
+    DotLinkedList dotList;
+    DotPanel dotPanel;
+    LineLinkedList lineList;
+     SquareLinkedList squareList;
 
-Tablero(){
-    //para tableros prederminados note que el x es multiplo de 100 de la cant de 
-    window = new JFrame();
-    window.setSize(1000, 1000);
-    window.setLayout(null);
-    window.setResizable(false);
-    window.setVisible(true);
-    PanelDots paneldots = new PanelDots(10, 10);
-    paneldots.setBounds(0, 0, 1000, 1000);
-    window.add(paneldots);
-    
-}
+    Tablero() {
+        window = new JFrame();
+        window.setSize(1000, 1000);
+        window.setLayout(null);
+        window.setResizable(false);
+        window.setVisible(true);
+        dotList = new DotLinkedList();
+        dotPanel = new DotPanel(dotList);
+        lineList = new LineLinkedList();
+        squareList = new SquareLinkedList();
+        dotPanel.setSize(1000, 1000);
+        window.add(dotPanel);
+        window.add(dotPanel, BorderLayout.CENTER);
 
-    class PanelDots extends JPanel{
-        private dot [][] matriz; 
-        private dot dotSeleccionado1;
-        private List<Line> lines = new ArrayList<>();
-        
-        public PanelDots(int filas, int columnas){
-            this.matriz = new dot[filas][columnas];
-            for(int i=0; i<filas; i++){
-                    for(int j=0; j<columnas; j++){
-                    matriz[i][j]= new dot(i*100,j*100); 
-                    // System.out.println("Printeo un punto");  
-                    
+        for (int i = 0; i < 10; i++) { // Crea puntos 10x10
+            for (int j = 0; j < 10; j++) {
+                Dot newDot = new Dot(i * 100, j * 100);
+                dotList.insertFirst(newDot);
             }
         }
-        
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                int x = e.getX();
-                int y = e.getY();
-        
-                // Buscar el dot clickeado
-                for (int i = 0; i < matriz.length; i++) {
-                    for (int j = 0; j < matriz[i].length; j++) {
-                        dot dotActual = matriz[i][j];
-                        if (Math.abs(x - dotActual.getX()) <= 20 && Math.abs(y - dotActual.getY()) <= 20) {
-                            if (dotSeleccionado1 == null) {
-                                dotSeleccionado1 = dotActual; // Establece el primer punto seleccionado
+    
+    }
+
+    class DotPanel extends JPanel {
+        private DotLinkedList dotList;
+        private Dot dotSeleccionado1;
+
+        public DotPanel(DotLinkedList dotList) {
+            this.dotList = dotList;
+            addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    int x = e.getX();
+                    int y = e.getY();
+                    Dot clickedDot = dotList.findDot(x, y);
+                    if (clickedDot != null) {
+                        if (dotSeleccionado1 == null) {
+                            dotSeleccionado1 = clickedDot;
+                        } else {
+                            if (Adjacent(dotSeleccionado1, clickedDot) && !lineExists(dotSeleccionado1, clickedDot)) {
+                                Line line = new Line(dotSeleccionado1, clickedDot);
+                                lineList.insertFirst(line); 
+                                dotSeleccionado1 = null;
+                                checkForSquares();
+                                repaint(); 
                             } else {
-                                // Check if the start and end points are adjacent
-                                if ((Adyacentes(dotSeleccionado1, dotActual))) {
-                                    // Check if a line already exists between these two dots
-                                    if (!lineExists(dotSeleccionado1, dotActual)) {
-                                        // Create a new line using the two selected points
-                                        Line nuevaLinea = new Line(dotSeleccionado1, dotActual);
-                                        lines.add(nuevaLinea);
-                                        dotSeleccionado1 = null; // Reinicia la selección del primer punto
-                                        repaint(); // Vuelve a dibujar el panel para mostrar la nueva línea
-                                    }
-                                    else{
-                                        System.out.println("Esa línea ya existe");
-                                        dotActual = null;
-                                        dotSeleccionado1 = null;
-                                        
-                                    }
-                                } else {
-                                    System.out.println("Picha no adyacente");
-                                    dotActual = null;
-                                    dotSeleccionado1 = null;
-                                }
+                                System.out.println("No se puede agregar la línea");
+                                dotSeleccionado1 = null;
                             }
                         }
+                        
                     }
                 }
+            });
+        }
+    
+        private boolean Adjacent(Dot dot1, Dot dot2) {
+            return Math.abs(dot1.getX() - dot2.getX()) == 100 && dot1.getY() == dot2.getY() ||
+                    Math.abs(dot1.getY() - dot2.getY()) == 100 && dot1.getX() == dot2.getX();
+        }
+
+        private boolean lineExists(Dot dot1, Dot dot2) {
+            Node current = lineList.getHead();
+            while (current != null) {
+                Line line = (Line) current.getData();
+                Dot lineDot1 = line.getDot1();
+                Dot lineDot2 = line.getDot2();
+                if ((lineDot1 == dot1 && lineDot2 == dot2) || (lineDot1 == dot2 && lineDot2 == dot1)) {
+                    return true;
+                }
+                
+                current = current.getNext();
             }
-        });
-    }
-
-private boolean lineExists(dot dot1, dot dot2) {
-    for (Line linea : lines) {
-        if ((linea.getStartDot() == dot1 && linea.getEndDot() == dot2) ||
-            (linea.getStartDot() == dot2 && linea.getEndDot() == dot1)) {
-            return true; // A line already exists between these two dots
+            return false;
+            
         }
-    }
-    return false; // No line exists between these two dots
-}
 
-private boolean Adyacentes(dot dot1, dot dot2) {
-    int x1 = dot1.getX();
-    int y1 = dot1.getY();
-    int x2 = dot2.getX();
-    int y2 = dot2.getY();
-
-    return Math.abs(x1 - x2) == 100 && y1 == y2 || Math.abs(y1 - y2) == 100 && x1 == x2;
-}
-    protected void paintComponent(Graphics g){
-        super.paintComponent(g);
-        for(int i=0; i<matriz.length; i++){
-                    for(int j=0; j<matriz[i].length; j++){
-                        g.drawOval(matriz[i][j].getX(), matriz[i][j].getY(), 5, 5);
+        private void checkForSquares() {
+            System.out.println("La función se ejecutó");
+            Node current = dotList.getHead();
+    
+            while (current != null) {
+                Dot dot = (Dot) current.getData();
+                int x = dot.getX();
+                int y = dot.getY();
+                Dot rightDot = dotList.findDot(x + 100, y);
+                Dot bottomDot = dotList.findDot(x, y + 100);
+                Dot bottomRightDot = dotList.findDot(x + 100, y + 100);
+    
+                if (rightDot != null && bottomDot != null && bottomRightDot != null) {
+                    if (lineExists(dot, rightDot) && lineExists(rightDot, bottomRightDot) &&
+                        lineExists(bottomRightDot, bottomDot) && lineExists(bottomDot, dot)) {
+                        System.out.println("Cuadrado Hecho");
+                        Square square = new Square(dot, rightDot, bottomDot, bottomRightDot);
+                        squareList.insertFirst(square);
                     }
                 }
-                for (Line linea : lines) {
-                    g.drawLine(linea.getStartDot().getX(), linea.getStartDot().getY(), 
-                               linea.getEndDot().getX(), linea.getEndDot().getY());
+    
+                current = current.getNext();
+            }
+    
+        }
+        public void mouseClicked(MouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+            Dot clickedDot = dotList.findDot(x, y);
+            if (clickedDot != null) {
+                if (dotSeleccionado1 == null) {
+                    dotSeleccionado1 = clickedDot;
+                } else {
+                    if (Adjacent(dotSeleccionado1, clickedDot) && !lineExists(dotSeleccionado1, clickedDot)) {
+                        
+                        Line line = new Line(dotSeleccionado1, clickedDot);
+                        dotList.insertFirst(line);
+                        dotSeleccionado1 = null;
+                        checkForSquares();
+                       
+                    } else {
+                        System.out.println("No se puede agregar la línea");
+                        dotSeleccionado1 = null;
+                    }
                 }
-    }
-    }
-    
-    class dot {
-        private int x;
-        private int y;
-    
-        public dot(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    
-        // Getters y setters
-        public int getX() {
-            return x;
-        }
-    
-        public void setX(int x) {
-            this.x = x;
-        }
-    
-        public int getY() {
-            return y;
-        }
-    
-        public void setY(int y) {
-            this.y = y;
+                repaint();
+            }
         }
     
         @Override
-        public String toString() {
-            return "(" + x + ", " + y + ")";
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            
+            Node squareCurrent = squareList.getHead();
+            while (squareCurrent != null) {
+                Square square = (Square) squareCurrent.getData();
+                Dot upperLeft = square.getUpperLeft();
+                int x1 = upperLeft.getX();
+                int y1 = upperLeft.getY();
+                g.fillRect(x1, y1, 98, 98); // You can adjust the size as needed
+            squareCurrent = squareCurrent.getNext();
+    }
+
+            Node current = dotList.getHead();
+            g.setColor(java.awt.Color.BLUE);
+            while (current != null) {
+                Dot dot = (Dot) current.getData();
+                int x = dot.getX();
+                int y = dot.getY();
+                g.fillOval(x, y, 5, 5); // Pinta un círculo (puedes ajustar el tamaño)
+                current = current.getNext();
+            }
+
+            Node lineNode = lineList.getHead();
+            while (lineNode != null) {
+                Line line = (Line) lineNode.getData();
+                int x1 = line.getX1();
+             int y1 = line.getY1();
+             int x2 = line.getX2();
+                int y2 = line.getY2();
+            g.drawLine(x1, y1, x2, y2);
+            lineNode = lineNode.getNext();
+        }
+
+        
         }
     }
 
+    class Dot {
+        private int x;
+        private int y;
+
+        public Dot(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
+    }
+
+    class Node {
+        private Object data;
+        private Node next;
+        public Node(Object data) {
+        this.next = null;
+        this.data = data;
+        }
+        public Object getData() {
+        return this.data;
+        }
+        public void setData(Object data) {
+        this.data = data;
+        }
+
+        public Node getNext() {
+            return this.next;
+            }
+            public void setNext(Node node) {
+            this.next = node;
+            }
+    
+    }
+
+    class DotLinkedList {
+        private Node head;
+        private int size;
+    
+        public DotLinkedList() {
+            this.head = null;
+            this.size = 0;
+        }
+    
+        public int size() {
+            return this.size;
+        }
+    
+        public void insertFirst(Object data) {
+            Node newNode = new Node(data);
+            newNode.setNext(this.head);
+            this.head = newNode;
+            this.size++;
+        }
+    
+        public Node getHead() {
+            return this.head;
+        }
+     
+        public Dot findDot(int x, int y) {
+            Node current = head;
+            while (current != null) {
+                Dot dot = (Dot) current.getData();
+                // Comprueba si las coordenadas (x, y) están dentro del rango del Dot
+                if (x >= dot.getX() && x <= dot.getX() + 25 // 5 es el tamaño del Dot en este ejemplo
+                    && y >= dot.getY() && y <= dot.getY() + 25) {
+                    return dot; // Devuelve el Dot si se encuentra dentro del rango
+                }
+                current = current.getNext();
+            }
+            return null; // Devuelve null si no se encuentra ningún Dot en las coordenadas (x, y)
+        }
+    
+    }
+
     class Line {
-        private dot startDot;
-        private dot endDot;
+        private Dot dot1;
+        private Dot dot2;
     
-        public Line(dot startDot, dot endDot) {
-            this.startDot = startDot;
-            this.endDot = endDot;
+        public Line(Dot dot1, Dot dot2) {
+            this.dot1 = dot1;
+            this.dot2 = dot2;
         }
     
-        public dot getStartDot() {
-            System.out.println("Se marco un dot");
-            return startDot;
-            
+        public Dot getDot1() {
+            return dot1;
         }
     
-        public dot getEndDot() {
-            return endDot;
+        public Dot getDot2() {
+            return dot2;
+        }
+
+        public int getX1() {
+            return dot1.getX();
+        }
+
+        public int getY1() {
+            return dot1.getY();
+        }
+
+        public int getX2() {
+            return dot2.getX();
+        }
+
+        public int getY2() {
+            return dot2.getY();
+        }
+    }
+
+    class LineLinkedList {
+        private Node head;
+        private int size;
+
+        public LineLinkedList() {
+            this.head = null;
+            this.size = 0;
+        }
+
+
+        public int size() {
+            return this.size;
+        }
+
+        public void insertFirst(Object data) {
+            Node newNode = new Node(data);
+            newNode.setNext(this.head);
+            this.head = newNode;
+            this.size++;
+        }
+
+        public Node getHead() {
+            return this.head;
+        }
+    }
+
+    class Square {
+        private Dot upperLeft;
+        private Dot upperRight;
+        private Dot lowerLeft;
+        private Dot lowerRight;
+    
+        public Square(Dot upperLeft, Dot upperRight, Dot lowerLeft, Dot lowerRight) {
+            this.upperLeft = upperLeft;
+            this.upperRight = upperRight;
+            this.lowerLeft = lowerLeft;
+            this.lowerRight = lowerRight;
+        }
+    
+       
+        public Dot getUpperLeft() {
+            return upperLeft;
+        }
+    
+        public Dot getUpperRight() {
+            return upperRight;
+        }
+    
+        public Dot getLowerLeft() {
+            return lowerLeft;
+        }
+    
+        public Dot getLowerRight() {
+            return lowerRight;
+        }
+    }
+
+    class SquareLinkedList {
+        private Node head;
+        private int size;
+    
+        public SquareLinkedList() {
+            this.head = null;
+            this.size = 0;
+        }
+    
+ 
+        public int size() {
+            return this.size;
+        }
+    
+        public void insertFirst(Square square) {
+            Node newNode = new Node(square);
+            newNode.setNext(this.head);
+            this.head = newNode;
+            this.size++;
+        }
+    
+        public Node getHead() {
+            return this.head;
         }
     }
 
     public static void main(String[] args) {
-       SwingUtilities.invokeLater(()->new Tablero());
-       
+        SwingUtilities.invokeLater(() -> new Tablero());
     }
 }
